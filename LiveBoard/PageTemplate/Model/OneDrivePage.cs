6874e53289f03b2a36e7e357af7995968d5e1c50
YoungjaeKim <youngjae.kim@gmail.com>
2014-03-11 18:37:05 +0900

    페북 템플릿 디자인 수정.

---
+++ LiveBoard/PageTemplate/Model/OneDrivePage.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using GalaSoft.MvvmLight;
+using Microsoft.Live;
+
+namespace LiveBoard.PageTemplate.Model
+{
+	public class OneDrivePage : ObservableObject, IPage
+	{
+		public OneDrivePage()
+		{
+			Guid = new Guid().ToString();
+
+			if (App.Session == null)
+			{
+				InitAuth();
+			}
+		}
+
+
+		private async void InitAuth()
+		{
+			if (!Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+			{
+				var authClient = new LiveAuthClient();
+				LiveLoginResult authResult =
+					await authClient.LoginAsync(new List<string>() { "wl.signin", "wl.basic", "wl.skydrive" });
+				if (authResult.Status == LiveConnectSessionStatus.Connected)
+				{
+					// An app-level property for the session.
+					App.Session = authResult.Session;
+				}
+			}
+		}
+
+
+		public string Guid { get; set; }
+
+		public string Title
+		{
+			get { return _title; }
+			set
+			{
+				_title = value;
+				RaisePropertyChanged("Title");
+			}
+		}
+
+		public string View { get; set; }
+		public string ViewOption { get; set; }
+
+		public TimeSpan Duration
+		{
+			get { return _duration; }
+			set
+			{
+				_duration = value;
+				RaisePropertyChanged("Duration");
+			}
+		}
+
+		public string Description
+		{
+			get { return _description; }
+			set
+			{
+				_description = value;
+				RaisePropertyChanged("Description");
+			}
+		}
+
+		public bool IsVisible { get; set; }
+		public string TemplateKey { get; set; }
+
+		public virtual async Task<bool> PrepareToLoadAsync()
+		{
+			// do nothing.
+			return true;
+		}
+
+		public XElement ToXml()
+		{
+			var xElement = new XElement("Page",
+				new XAttribute("Title", Title ?? ""),
+				new XAttribute("IsVisible", IsVisible),
+				new XAttribute("Description", Description ?? ""),
+				new XAttribute("Guid", Guid ?? new Guid().ToString()),
+				new XAttribute("Duration", Duration.TotalMilliseconds),
+				new XAttribute("TemplateKey", TemplateKey ?? ""),
+				new XAttribute("View", View ?? ""),
+				new XAttribute("ViewOption", ViewOption ?? ""),
+				new XElement("DataList", Data.Select(d => d.ToXml())));
+			return xElement;
+		}
+
+		/// <summary>
+		/// Specific data.
+		/// </summary>
+		public IEnumerable<LbPageData> Data
+		{
+			get { return _data; }
+			set
+			{
+				_data = value;
+				RaisePropertyChanged("Data");
+			}
+		}
+
+		public override string ToString()
+		{
+			return String.Format("{0} ({1})", Title, View);
+		}
+
+		private IEnumerable<LbPageData> _data;
+		private string _title;
+		private TimeSpan _duration;
+		private string _description;
+	}
+}
+
---
+++ LiveBoard/View/AccountSettingsFlyout.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Settings Flyout item template is documented at http://go.microsoft.com/fwlink/?LinkId=273769
+using Microsoft.Live;
+
+namespace LiveBoard.View
+{
+	// http://msdn.microsoft.com/en-us/library/live/hh968445.aspx#settings 참고.
+
+	public sealed partial class AccountSettingsFlyout : SettingsFlyout
+	{
+		public AccountSettingsFlyout()
+		{
+			this.InitializeComponent();
+
+			SetNameField(false);
+			//Task.Run(async () =>
+			//{
+			//	await SetNameField(false);
+			//});
+		}
+
+		private async void SignInButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			await SetNameField(true);
+		}
+
+		private async void SignOutButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				// Initialize access to the Live Connect SDK.
+				LiveAuthClient LCAuth = new LiveAuthClient();
+				LiveLoginResult LCLoginResult = await LCAuth.InitializeAsync();
+				// Sign the user out, if he or she is connected;
+				//  if not connected, skip this and just update the UI
+				if (LCLoginResult.Status == LiveConnectSessionStatus.Connected)
+				{
+					LCAuth.Logout();
+				}
+
+				// At this point, the user should be disconnected and signed out, so
+				//  update the UI.
+				var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+				this.UserNameTextBlock.Text = loader.GetString("MicrosoftAccount.Text");
+
+				// Show sign-in button.
+				SignInButton.Visibility = Visibility.Visible;
+				SignOutButton.Visibility = Visibility.Collapsed;
+			}
+			catch (LiveConnectException x)
+			{
+				// Handle exception.
+			}
+		}
+
+		private async Task SetNameField(Boolean login)
+		{
+			// If login == false, just update the name field. 
+			await App.updateUserName(this.UserNameTextBlock, login);
+
+			// Test to see if the user can sign out.
+			Boolean userCanSignOut = true;
+
+			var LCAuth = new LiveAuthClient();
+			LiveLoginResult LCLoginResult = await LCAuth.InitializeAsync();
+
+			if (LCLoginResult.Status == LiveConnectSessionStatus.Connected)
+			{
+				userCanSignOut = LCAuth.CanLogout;
+			}
+
+			var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+
+			if (String.IsNullOrEmpty(UserNameTextBlock.Text) 
+				|| UserNameTextBlock.Text.Equals(loader.GetString("MicrosoftAccount.Text")))
+			{
+				// Show sign-in button.
+				SignInButton.Visibility = Visibility.Visible;
+				SignOutButton.Visibility = Visibility.Collapsed;
+			}
+			else
+			{
+				// Show sign-out button if they can sign out.
+				SignOutButton.Visibility = userCanSignOut ? Visibility.Visible : Visibility.Collapsed;
+				SignInButton.Visibility = Visibility.Collapsed;
+			}
+		}
+	}
+}
